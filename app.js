cd8c055b6ece61c9b8abb815d5c88ecae5c6e780
shashi <shashi@gmail.com>
2018-05-24 09:49:59 +0530

    this is my first coimmit

---
+++ app.js
@@ -0,0 +1,91 @@
+// var username = prompt("enter your name");
+
+// if(username !== 'null'){
+//     document.write("my Name is " + username)
+// }else {
+//     document.write("user name is not printing here");
+// }
+
+
+// var number = 2;
+
+// switch(number){
+//     case 1:
+//         document.write("won tv");
+//             break;
+//     case 2:
+//         document.write("won music player");
+//             break;
+//     default:
+//         document.write("try next time");
+
+
+// }
+
+
+var array = ["java" , "python" , "javascript" , "nodejs"];
+
+// console.log(x.length);
+    // console.log(array[0]);
+    // console.log(array[1]);
+    // console.log(array[2]);
+    // console.log(array[3]);
+
+// for(var i=0; i<=array.length; i++){
+//     console.log(array[i]);
+// }
+
+
+
+// for(var i=0; i<=100; i++){
+//     if(i === 10){
+//         break;
+//     }
+// }
+// for(var i=0; i<=100; i++){
+//     if(i === 10 , i===20){
+//         continue;
+//     }
+// }
+
+
+// array.forEach(function(item){
+//     console.log(item);
+// })
+
+// var obj = {
+//     name : "shashi",
+//     age : 30,
+//     skill:"javascript"
+// }
+
+// for(let i in obj){
+//     console.log(i + ": " +  obj[i]);
+// }
+
+
+// var array1 = [1 , 10 , 4 , 100 , 40];
+
+// for(var i of array1){
+//     console.log(i);
+// }
+
+testFun();
+
+function testFun(){
+    alert("hello");
+}
+
+
+
+
+// var test = function(){
+//     alert("hello");
+// }
+// test();
+
+// test();
+
+// (function(){
+//     alert("hello")
+// }())
